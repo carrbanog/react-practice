94c4c80526def0f59c2dec728d7d8fe063df3814
carrbanog <loopang5954@gmail.com>
2024-09-03 13:43:06 +0900

    rendering

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import User from "./user";
+import getImageUrl from "./user";
+import recipes from "./data";
+
+function Recipe({ menu }) {
+  return (
+    <>
+      {menu.map((e) => (
+        <div key={e.id}>
+          <h3>{e.name}</h3>
+          {e.ingredients.map((e) => (
+            <li key={e}>{e}</li>
+          ))}
+        </div>
+      ))}
+    </>
+  );
+}
 
 function App() {
+  const salad = recipes.filter((e) => e.name === "Greek Salad");
+  const pizza = recipes.filter((e) => e.name === "Hawaiian Pizza");
+  const hummus = recipes.filter((e) => e.name === "Hummus");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Recipes</h1>
+      <Recipe menu={salad} />
+      <Recipe menu={pizza} />
+      <Recipe menu={hummus} />
     </div>
   );
 }
